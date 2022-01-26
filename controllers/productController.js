68a97678e3effe69ce5132503484d0b029b4db64
3ue4 <3ue4@fresnou.org>
2022-01-26 08:01:40 -0800

    initial commit

---
+++ controllers/productController.js
@@ -0,0 +1,17 @@
+const {Product} = require('../models')
+const categories = ['appliances', 'pets', 'home goods', 'other'];
+
+module.exports.renderProfile = async function(req,res){
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/profile', {product});
+}
+
+module.exports.renderEditForm = async function (req, res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit', {product, categories});
+}
+
